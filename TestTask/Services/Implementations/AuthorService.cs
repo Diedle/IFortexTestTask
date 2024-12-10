5e0deb435be9c0d34f98575baabd59406ebd0bee
LEVCHUK <diedle101000@gmail.com>
2024-12-10 16:27:15 +0300

    Добавьте файлы проекта.

---
+++ TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    /// <summary>
+    /// Implementation of the IAuthorService interface.
+    /// Provides methods to interact with author data.
+    /// </summary>
+    public class AuthorService : IAuthorService
+    {
+        private readonly ApplicationDbContext _context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuthorService"/> class.
+        /// </summary>
+        /// <param name="context">The application database context.</param>
+        public AuthorService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Gets the author who has written the book with the longest title.
+        /// If multiple authors have written books with the same title length,
+        /// the author with the smallest ID is returned.
+        /// </summary>
+        /// <returns>The <see cref="Author"/> with the longest book title, or null if no authors are found.</returns>
+        public async Task<Author> GetAuthor()
+        {
+            int maxLength = await _context.Books.MaxAsync(b => b.Title.Length);
+
+            var author = await _context.Authors
+                .Where(a => a.Books.Any(b => b.Title.Length == maxLength))
+                .OrderBy(a => a.Id)
+                .FirstOrDefaultAsync();
+
+            return author;
+        }
+
+
+        /// <summary>
+        /// Gets the list of authors who have written an even number of books
+        /// published after the year 2015.
+        /// </summary>
+        /// <returns>A list of <see cref="Author"/> objects.</returns>
+        public async Task<List<Author>> GetAuthors()
+        {
+            var authorIds = await _context.Books
+                .Where(b => b.PublishDate.Year > 2015)
+                .GroupBy(b => b.AuthorId)
+                .Where(g => g.Count() % 2 == 0)
+                .Select(g => g.Key)
+                .ToListAsync();
+            var authors = await _context.Authors
+                .Where(a => authorIds.Contains(a.Id))
+                .ToListAsync();
+            return authors;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    /// <summary>
+    /// Implementation of the IBookService interface.
+    /// Provides methods to interact with book data.
+    /// </summary>
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DateTime _carolusRexDate = new DateTime(2012, 5, 25);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BookService"/> class.
+        /// </summary>
+        /// <param name="context">The application database context.</param>
+        public BookService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        /// <summary>
+        /// Gets the book with the highest published cost (price multiplied by quantity published).
+        /// </summary>
+        /// <returns>The <see cref="Book"/> with the highest published cost, or null if no books are found.</returns>
+        public async Task<Book> GetBook()
+        {
+            return await _context.Books
+                .OrderByDescending(b => b.Price * b.QuantityPublished)
+                .FirstOrDefaultAsync();
+        }
+
+        /// <summary>
+        /// Receives a list of books whose title contains the word "Red" and 
+        /// which were published after the release date of the album "Carolus Rex" by the Sabaton group.
+        /// </summary>
+        /// <returns>A list of <see cref="Book"/> objects.</returns>
+        public async Task<List<Book>> GetBooks()
+        {
+            return await _context.Books
+                .Where(b => b.Title.Contains("Red") && b.PublishDate > _carolusRexDate)
+                .ToListAsync();
+        }
+    }
+}
